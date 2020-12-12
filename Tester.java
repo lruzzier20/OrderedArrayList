36db352db60ecebec9d21663c18fa12c045bb6a2
Logan <lruzzier20@stuy.edu>
2020-12-12 17:57:59 -0500

    added second constructor and test cases

--- Tester.java
+++ Tester.java
@@ -1,8 +1,11 @@
 public class Tester{
   public static void main(String[] args){
     NoNullArrayList<Integer> a = new NoNullArrayList<Integer>();
+    NoNullArrayList<String> b = new NoNullArrayList<String>(3);
     a.add(1);
-    a.add(null);
     System.out.println(a);
+    a.add(0, null);
+    System.out.println(a);
+    System.out.println(b);
   }
 }
