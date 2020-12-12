c5d323a5f30c0acc9055fed12ba4e5687b1c4746
Logan <lruzzier20@stuy.edu>
2020-12-12 18:10:20 -0500

    overrode set() and added test methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -19,4 +19,11 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     if(element==null){throw new IllegalArgumentException("no nulls");}
     super.add(index, element);
   }
+
+  public T set(int index, T element){
+    if(element==null){throw new IllegalArgumentException("no nulls");}
+    T temp=this.get(index);
+    super.set(index, element);
+    return temp;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
 public class Tester{
   public static void main(String[] args){
     NoNullArrayList<Integer> a = new NoNullArrayList<Integer>();
     NoNullArrayList<String> b = new NoNullArrayList<String>(3);
+    ArrayList<Integer> test = new ArrayList<>();
+    test.add(15);
     a.add(1);
     System.out.println(a);
-    a.add(0, null);
+    a.add(0, 3);
     System.out.println(a);
+    System.out.println(b);
+    System.out.println(test);
+    System.out.println(test.set(0, 20));
+    System.out.println(test);
+    System.out.println(b.add("Hello!"));
+    System.out.println(b.set(0, "You Shouldn't Be Seeing This"));
     System.out.println(b);
   }
 }
