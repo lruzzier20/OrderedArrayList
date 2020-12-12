f89c257bbea03eee2b4a6ccbd5e999773b3fcd91
Logan <lruzzier20@stuy.edu>
2020-12-12 17:51:34 -0500

    classwork-- added a constructor and add()

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
+import java.lang.IllegalArgumentException;
 public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(){
     super();
+  }
+
+  public boolean add(T element){
+    if(element==null){throw new IllegalArgumentException("no nulls");}
+    super.add(element);
+    return true;
   }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,8 @@
 public class Tester{
   public static void main(String[] args){
     NoNullArrayList<Integer> a = new NoNullArrayList<Integer>();
+    a.add(1);
+    a.add(null);
+    System.out.println(a);
   }
 }
